7750ecedec3090b35880cf3b50658324839494f4
mx44 <jeongbinlee44@gmail.com>
2022-04-07 19:22:04 +1000

    final version for assessment 2

--- assignment2/assign2_template-v3.c
+++ assignment2/assign2_template-v3.c
@@ -3,8 +3,8 @@
 //            *************NOTE**************
 // This is a template for the subject of RTOS in University of Technology Sydney(UTS)
 // Please complete the code based on the assignment requirement.
-// Author: Jeong Bin Lee
-// Student ID: 12935084
+// Author: Jeong Bin Lee  | Student ID: 12935084
+// Author: Quang Le       | Student ID: 11993654
 //***********************************************************************************
 /***********************************************************************************/
 
@@ -32,20 +32,15 @@ typedef struct ThreadParams {
   int pipeFile[2];
   sem_t sem_A, sem_B, sem_C;
   char message[MAX_BUFFER];
-  pthread_mutex_t lock;
+  // pthread_mutex_t lock;
 } ThreadParams;
 
 /* * * * * * * * * * GLOBAL VARIABLES * * * * * * * * * */
 pthread_attr_t attr;
-int counter; /* buffer counter This is the global variable might cause critical section*/
-int Max_counter; /* hold the Max counter unmber for aligned printing as input*/
 int fd[2];//File descriptor for creating a pipe
-
-char *input_file;
+char *input_file;  /* used to hold the file name passed in from command line */
 
 /* --- Prototypes --- */
-int insert_item(ThreadParams *p, char item);
-int remove_item(ThreadParams *p, char *item);
 
 
 /* Initializes data and utilities used in thread params */
@@ -69,11 +64,7 @@ int main(int argc, char const *argv[])
 
   if(argc < 2)
   {
-    fprintf(stderr,"usage: input.txt <file name>\n");
-    return -1;
-  }
-  if(argv < 0)
-  {
+    fprintf(stderr,"usage: input.txt <file name> (for example: ./assignment2 data.txt)\n");
     fprintf(stderr,"%s \n", *argv);
     return -1;
   }
@@ -92,7 +83,6 @@ int main(int argc, char const *argv[])
   }
 
   // Create Threads
-  // pthread_create(&(tid[0]), &attr, &ThreadA, (void*)(&params));  // resource is not sufficient (may require change)
   if(pthread_create(&(tid[0]), &attr, &ThreadA, (void*)(&params)) != 0) 
   { 
     perror ("ERROR MAIN: failed create thread A\n"); 
@@ -111,15 +101,10 @@ int main(int argc, char const *argv[])
     exit (1);
   }
 
-  //TODO: add your code
-
   // Wait on threads to finish
   pthread_join(tid[0], NULL);  // Thread A
   pthread_join(tid[1], NULL);  // Thread B
   pthread_join(tid[2], NULL);  // Thread C
-
-  
-  //TODO: add your code
 
   return 0;
 }
@@ -131,28 +116,21 @@ void initializeData(ThreadParams *params) {
   sem_init(&(params->sem_C), 0, 0);  // initialise but dont signal
   // Initialize thread attributes 
   pthread_attr_init(&attr);
-  //TODO: add your code
 
   // Initialise mutex lock
-  pthread_mutex_init(&(params->lock), NULL);
-
-  counter = 0; 
-  Max_counter = 0;
-  return;
+  // pthread_mutex_init(&(params->lock), NULL);
 }
 
 void *ThreadA(void *params)
 {
-  //TODO: add your code
   ThreadParams *p = params;
   FILE *pfile;
   char c[MAX_BUFFER];
-  // char file_name[50] = "data.txt";
   char eof[4] = "EOF";  // describes the end of file
   int result;
 
   printf("/****** RUNNING THREAD A ******/\n");
-  printf("\nthread A read from data.txt\n");
+  printf("thread A read from data.txt\n");
   
   if (input_file == NULL)
   {
@@ -196,9 +174,8 @@ void *ThreadA(void *params)
       printf("DEBUG A: Closing data.txt!\n");
       fclose(pfile);
       result=write(fd[1], &eof, MAX_BUFFER);
-      sem_post(&(p->sem_B));
       printf("/****** END OF THREAD A ******/\n");
-      exit(0);
+      sem_post(&(p->sem_B));
     }
   }
 }
@@ -209,21 +186,16 @@ void *ThreadB(void *params)
   ThreadParams *p = params;
   char buff[MAX_BUFFER];
   int result;
-  // int index;
 
   printf("/****** RUNNING THREAD B ******/\n");
-  //TODO: add your code
-  printf("\nthread B reading from thread A\n");
+  printf("thread B reading from thread A\n");
   
   /******************** THREAD B MAIN LOOP ********************/
   while(1)
   {
     // wait for semaphore
     sem_wait(&(p->sem_B));
-    // index=0;
     result = read(fd[0], buff, MAX_BUFFER);
-    /***************** PRINT THREADB HERE  *****************/
-    // printf("DEBUG B: result: %d\n", result);
     printf("DEBUG B: buff: %s", buff);
     if (result != MAX_BUFFER)
     {
@@ -235,24 +207,19 @@ void *ThreadB(void *params)
     if (!strcmp(buff, "EOF"))
     {
       printf("\nDEBUG B: reading pipe has completed\n");
-      // pthread_mutex_lock(&p->lock);
-      // insert_item(params, '\0');
-      // insert_item(params, 2);
       p->message[0] = 2;
-      // pthread_mutex_unlock(&p->lock);
+      printf("/****** END OF THREAD B ******/\n");
       sem_post(&(p->sem_C));
-      break;
     }
-
-    for (int i=0; i<255; i++)
+    else
     {
-      p->message[i] = buff[i];
+      for (int i=0; i<255; i++)
+      {
+        p->message[i] = buff[i];
+      }
+      sem_post(&(p->sem_C));
     }
-
-    sem_post(&(p->sem_C));
   }
-  printf("/****** END OF THREAD B ******/\n");
-  exit(0);
 }
 
 
@@ -263,8 +230,7 @@ void *ThreadC(void *params)
   int write_to_file = 0;
 
   printf("/****** RUNNING THREAD C ******/\n");
-  //TODO: add your code
-  printf("\nThread C read from Thread B\n");
+  printf("Thread C read from Thread B\n");
 
   pfile_out = fopen("output.txt", "w");
   if (pfile_out == NULL)
@@ -278,18 +244,8 @@ void *ThreadC(void *params)
   {
     // wait for semaphore
     sem_wait(&(p->sem_C));
-    if (write_to_file == 1 && p->message[0] != 2)
-    {
-      fputs(p->message, pfile_out);
-    }
-    if (strstr(p->message, "end_header") != NULL && write_to_file == 0)
-    {
-      printf("DEBUG C: Writing to file!\n");
-      write_to_file = 1;
-    }
 
-
-    if (counter == 0 && p->message[0] == 2)
+    if (p->message[0] == 2)
     {
       printf("/****** END OF THREAD C ******/\n");
       /***************** PRINT THREADC HERE *****************/
@@ -301,49 +257,24 @@ void *ThreadC(void *params)
       fclose(pfile_out);
       exit(0);
     }
-    else
+
+    if (write_to_file == 1)
     {
-      /***************** PRINT THREADC HERE *****************/
-      printf("DEBUG C: p->message: %s\n", p->message);
-      memset(p->message, 0, MAX_BUFFER);
-      sem_post(&p->sem_A);
+      fputs(p->message, pfile_out);
     }
-  }
-}
+    
+    if (strstr(p->message, "end_header\n") != NULL && write_to_file == 0)
+    {
+      printf("DEBUG C: Writing to file!\n");
+      write_to_file = 1;
+    }
 
-/* Add an item to the buffer */
-int insert_item(ThreadParams *p, char item)
-{
-   /* When the buffer is not full add the item
-      and increment the counter*/
-  if(counter < MAX_BUFFER) 
-  {
-    p->message[counter] = (int)item;
-    counter++;
-    return 0;
-  }
-  else 
-  { /* Error the buffer is full */
-    return -1;
-  }
-}
-
-/* Remove an item from the buffer */
-int remove_item(ThreadParams *p, char *item)
-{
-  /* When the buffer is not empty remove the item
-     and decrement the counter */
-  if(counter > 0)
-  {
-    *item = p->message[(Max_counter-counter)];
-    counter--;
-    return 0;
-  }
-  else 
-  { /* Error buffer empty */
-    return -1;
+    /***************** PRINT THREADC HERE *****************/
+    printf("DEBUG C: p->message: %s\n", p->message);
+    memset(p->message, 0, MAX_BUFFER);
+    sem_post(&p->sem_A);
   }
 }
 
 
-
+/* END OF FILE */
